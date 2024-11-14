11c7d005ae39aecc42841db0fc527d3b5b8ffed6
trplx666 <russiansoveet@gmail.com>
2024-11-14 19:08:14 +0300

    init

---
+++ src/main/java/com/case_battle/springcasebattleparser/SpringCaseBattleParserApplication.java
@@ -0,0 +1,13 @@
+package com.case_battle.springcasebattleparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCaseBattleParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCaseBattleParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/case_battle/springcasebattleparser/SpringCaseBattleParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_battle.springcasebattleparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCaseBattleParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
